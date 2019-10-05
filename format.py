7f9ed6a6557123f0fa9e2c9a1def33a42a2c4335
dinith72 <jdinith72@gmail.com>
2019-10-05 23:08:10 +0530

    format music initital commit

---
+++ format.py
@@ -0,0 +1,34 @@
+import re
+import os 
+from shutil import copyfile
+
+srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Desktop\\toBeProcessed'
+# srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Music\\vol1'
+artistNames = ['gunadasa', 'kapuge' ,'clarance' ,'wijewardana', 'daddy' ,'damith' , 'asanka' , 
+'athula' ,'adhikari','samitha' , 'vijaya', 'kumarathunga','amaredeva' , 'umaria' ]
+otherJunkWords = ['wwwsindume', 'wwwtopsinhalamp3', 'www' , 'top' , 'sinhala' , 'mp3' , 'sindu' , ]
+
+folder = os.listdir(srcPath)
+
+for file in folder:
+    if(file != 'desktop.ini'):
+        fileName = file
+        endPosition = len(fileName) - 4
+        modifiedName = fileName[0:endPosition]
+        modifiedName = re.sub(r'[0-9,-/+{}=,.]','',modifiedName).lower()
+        modifiedName = re.sub(r'[_]',' ',modifiedName)
+
+        for name in artistNames:
+            modifiedName = re.sub(name ,'',modifiedName)
+
+        for word in otherJunkWords:
+            modifiedName = re.sub(word , '', modifiedName)
+        
+        modifiedName =  modifiedName.strip()
+        os.rename(srcPath + '\\' + file,'./processed/' + modifiedName+'.mp3')
+        print(modifiedName)
+
+print('')
+
+
+
