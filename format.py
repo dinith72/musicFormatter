3f634b901165c7010a40e8ce094262d19df3ad98
dinith72 <jdinith72@gmail.com>
2019-10-05 23:51:19 +0530

    change the location of the src folder

--- format.py
+++ format.py
@@ -2,12 +2,12 @@ import re
 import os 
 from shutil import copyfile
 
-srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Desktop\\toBeProcessed'
-# srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Music\\vol1'
+# srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Desktop\\toBeProcessed'
+srcPath = 'C:\\Users\\Dinith Jaybaodhi\\Music\\toBeProcessed'
 artistNames = ['gunadasa', 'kapuge' ,'clarance' ,'wijewardana', 'daddy' ,'damith' , 'asanka' , 
 'athula' ,'adhikari','samitha' , 'vijaya', 'kumarathunga','amaredeva' , 'umaria', 'chandana' ,'liyanarachchi' , 'athma' ,
 'liyanage' , 'punsiri' , 'soysa', 'henry' , 'kaldera' , ' deepika'  ,'priyadharshani' , 'rodni' , 'warnakula'
-'kasun' , 'kalhara' , 'dayarathna' , 'ranathunga' , 'deepika'  , 'priyadharshani']
+'kasun' , 'kalhara' , 'dayarathna' , 'ranathunga' , 'deepika'  , 'priyadharshani', 'namal' , 'udugama']
 otherJunkWords = ['wwwsindume', 'wwwtopsinhalamp3', 'www' , 'top' , 'sinhala' , 'mp3' , 'sindu' , ]
 
 folder = os.listdir(srcPath)
